b7552b53da36e41e7ac7a13f19696b3280f87ec0
tmerli <theo.merli60@gmail.com>
2019-11-25 16:34:21 +0100

    not slow but wrong

--- logreg_train.py
+++ logreg_train.py
@@ -10,21 +10,23 @@ data = pd.read_csv('dataset_train.csv', index_col="Index",
                    usecols=['Index', 'Hogwarts House', 'Astronomy', 'Herbology', 'Divination', 'Muggle Studies', 'Ancient Runes',
                             'History of Magic', 'Transfiguration', 'Potions', 'Charms', 'Flying'])
 
-houses = data["Hogwarts House"]
-
-houseNames = ['Gryffyndor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
-
-data = data.drop('Hogwarts House', 1)
+houseNames = ['Gryffindor', 'Slytherin', 'Hufflepuff', 'Ravenclaw']
 
 data = data.dropna()
+
+houses = data["Hogwarts House"]
+
+data = data.drop('Hogwarts House', 1)
 
 data.insert(0, 'ones', 1)
 
 size = len(data.index)
 
-data = data.to_numpy()
+np.set_printoptions(threshold=np.inf)
+
 
 thetas = np.ones(11)
+
 
 def train(thetas, house):
     tmp = np.array(thetas)
@@ -32,12 +34,8 @@ def train(thetas, house):
     for h in houses:
         y.append(1 if h == house else 0)
     i = 0
-    while(i < 11):
-        summ = 0
-        index = 0
-        for row in data:
-            summ += (sigmoid(np.dot(thetas, row)) - y[index]) * row[i]
-            index += 1
+    for column in data.columns:
+        summ = np.sum((sigmoid(np.dot(data, thetas)) - y) * data[column])
         tmp[i] = 1/size * summ
         i += 1
     return tmp
@@ -47,7 +45,7 @@ thetas = []
 
 for house in houseNames:
     newThetas = np.zeros(11)
-    i = 50
+    i = 1
     while (i):
         i -= 1
         newThetas = train(newThetas, house)
